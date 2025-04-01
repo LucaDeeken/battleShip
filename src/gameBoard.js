0ad9b37d9a6e2f44e17aff60125c423de89c834e
Luca Deeken <luca.deeken@fuchs-edv.de>
2025-04-01 15:31:54 +0200

    add shipVariables to gameboardClass

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,4 +1,6 @@
-export  class Gameboard {
+import { Ship } from "./ship.js";
+
+export class Gameboard {
   constructor() {
     this.placeBoard = [];
     for (let i = 0; i < 10; i++) {
@@ -14,18 +16,28 @@ export  class Gameboard {
     }
     this.fireBoard = [];
     for (let i = 0; i < 10; i++) {
-        let leftArray = [];
-        for (let j = 0; j < 10; j++) {
-          let cords = {
-            coordinate: i + j,
-            hit: false,
-          };
-          leftArray.push(cords);
-        }
-        this.fireBoard.push(leftArray);
+      let leftArray = [];
+      for (let j = 0; j < 10; j++) {
+        let cords = {
+          coordinate: i + j,
+          hit: false,
+        };
+        leftArray.push(cords);
       }
+      this.fireBoard.push(leftArray);
+    }
+    this.ships = {
+        verySmall : new Ship(2),
+        smallOne : new Ship(3),
+        smallTwo : new Ship(3),
+        big : new Ship(4),
+        massive : new Ship(5),
+    }
+  }
+
+  receiveAttack() {}
+  
+  positionShip(chosenShip) {
+
   }
 }
-
-export const initGameboard = new Gameboard();
-console.log(initGameboard);
