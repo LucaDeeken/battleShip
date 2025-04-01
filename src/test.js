daaf10449462d579562a639b46ba64d96e471ad9
Luca Deeken <luca.deeken@fuchs-edv.de>
2025-04-01 12:33:18 +0200

    write tests for ship and gameboard objects

--- src/test.js
+++ src/test.js
@@ -1,5 +1,29 @@
-const sum = require('./index');
+import { Ship } from "./ship.js";
+import { Gameboard } from "./gameBoard.js";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe("Test Ship-Object", () => {
+  test("check if length-constructor works", () => {
+    const spyDummy = new Ship(5);
+    expect(spyDummy.length).toBe(5);
+  });
+  test("check if hit and sunk mechanism works", () => {
+    const spyDummy = new Ship(5);
+    expect(spyDummy.timesHit).toBe(0);
+    expect(spyDummy.sunk).toBe(false);
+    spyDummy.hit();
+    spyDummy.hit();
+    spyDummy.hit();
+    spyDummy.hit();
+    spyDummy.hit();
+    expect(spyDummy.timesHit).toBe(5);
+    spyDummy.isSunk();
+    expect(spyDummy.sunk).toBe(true);
+  });
+});
+
+describe("Test Gameboard-Object", () => {
+  test("check if array/object generation works", () => {
+    const spyDummy = new Gameboard();
+    expect(spyDummy.placeBoard.length).toBe(10);
+  });
 });
